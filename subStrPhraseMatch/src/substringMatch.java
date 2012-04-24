e09fd2b0f0af9cf794970e61cbe54cf141747cbe
Kou AMANO <amano.au1@gmail.com>
2012-04-24 20:30:58 +0900

    rev2

--- subStrPhraseMatch/src/substringMatch.java
+++ subStrPhraseMatch/src/substringMatch.java
@@ -2,7 +2,7 @@
 public class substringMatch {
 	public int substringmatchQ(String query, String base){
 		int r = 0;
-			if(base.matches(".*"+query+".*")){
+			if(base.matches("(?i)"+query+".*")){
 				r = 1;
 			}else{
 				r = 0;
